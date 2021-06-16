cd7f0ced825f22ebc7cc40974742403067274bf0
Dvij1309 <78919360+Dvij1309@users.noreply.github.com>
2021-06-16 22:03:36 +0530

    Add files via upload

---
+++ js/System.js
@@ -0,0 +1,17 @@
+class System{
+
+    constructor(){}
+
+    // Add code to authenticate the given code and the access codes.
+    authenticate(actualCode,enteredCode){
+        textSize(50);
+        text(code,300,300);
+        if(actualCode === enteredCode)
+        return true
+
+    else
+
+        return false 
+    }    
+
+}
